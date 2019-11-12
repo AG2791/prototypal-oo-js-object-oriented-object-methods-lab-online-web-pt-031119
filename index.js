e8e0281947054d7abce51212c68dda4913af6a52
AJ Guiles <ashiguiles@gmail.com>
2019-11-12 02:56:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,4 +22,8 @@ BoardMember.prototype.releasePressStatment = function() {
   
 }
 
+BoardMember.prototype.sayHi = function(){
+  return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+}
+
   
