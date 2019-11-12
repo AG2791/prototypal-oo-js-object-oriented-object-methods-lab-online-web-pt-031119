a3900a3a6f6d5f416063dab8142a10f24120cec7
AJ Guiles <ashiguiles@gmail.com>
2019-11-12 02:47:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 function Boardmember(name, Homestate, training) {
-  let veto = "No, I must disagree";
-  let approve = "You can do that!";
-  let doCharity = "I live to help people";
+  
   
   
   
